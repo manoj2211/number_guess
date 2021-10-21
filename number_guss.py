b0a77ea17f14851b2a0914f0fb301c4f600cf4fc
Manojkumar Ganesan <50042410+manoj2211@users.noreply.github.com>
2021-10-21 20:45:49 +0530

    Just space  removed

--- number_guss.py
+++ number_guss.py
@@ -2,10 +2,8 @@ import random
 import math
 # Taking Inputs
 lower = int(input("Enter Lower bound:- "))
-
 # Taking Inputs
 upper = int(input("Enter Upper bound:- "))
-
 # generating random number between
 # the lower and upper
 x = random.randint(lower, upper)
